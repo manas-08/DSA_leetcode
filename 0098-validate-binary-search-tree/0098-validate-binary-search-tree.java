7ab5776e4b4662f0620e2e482fa17113a3f48a7a
manas-08 <147643919+manas-08@users.noreply.github.com>
2025-05-13 22:29:22 +0530

    Time: 2 ms (13.16%), Space: 43.5 MB (59.63%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        if (root == null)
+            return true;
+        Stack<TreeNode> stk = new Stack<>();
+        TreeNode prev = null;
+        while (root != null || !stk.isEmpty()) {
+            while (root != null) {
+                stk.push(root);
+                root = root.left;
+            }
+            root = stk.pop();
+            if (prev != null && root.val <= prev.val)
+                return false;
+            prev = root;
+            root = root.right;
+        }
+        return true;
+    }
+}
