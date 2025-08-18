03b7b63ad4c145da8c491cedb9723284e7d5b914
manas-08 <147643919+manas-08@users.noreply.github.com>
2025-08-18 22:38:03 +0530

    Time: 0 ms (100%), Space: 43.4 MB (35.9%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -15,13 +15,24 @@
  */
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        //recursion approach
-        if (root == null){ 
-            return false;
-        }
-        if (root.left == null && root.right == null && root.val == targetSum){
-            return true;
-        }
-        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+        if(root == null && targetSum == 0) return false;
+        return helper(root, targetSum);
+    }
+    public boolean helper(TreeNode root, int targetSum){ 
+        if(root == null) return false;
+
+        if(root.left == null && root.right == null && targetSum == root.val) return true;
+
+        boolean left = hasPathSum(root.left, targetSum - root.val);
+        boolean right = hasPathSum(root.right, targetSum - root.val);
+        return left || right;
     }
 }
+
+// if (root == null){ 
+//             return false;
+//         }
+//         if (root.left == null && root.right == null && root.val == targetSum){
+//             return true;
+//         }
+//         return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
