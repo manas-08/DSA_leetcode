95634809945a4aa57a1ba5d505e7576554d573cf
manas-08 <147643919+manas-08@users.noreply.github.com>
2025-09-20 19:15:52 +0530

    Time: 601 ms (13.91%), Space: 0B (100%) - LeetHub

--- 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -1,5 +1,2 @@
-select customer_number
-from Orders
-group by customer_number
-order by count(customer_number) desc
-limit 1; 
+select customer_number from (select *, rank() over(order by count(customer_number) desc) as 'rank' from Orders group by customer_number) t
+where t.rank = 1;
